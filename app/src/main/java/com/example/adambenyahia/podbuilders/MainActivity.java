7f03cbb78549a02919c76f20ff8ce9f5517847e0
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-10-01 20:03:47 +0200

    other screen done

--- app/src/main/java/com/example/adambenyahia/podbuilders/MainActivity.java
+++ app/src/main/java/com/example/adambenyahia/podbuilders/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.adambenyahia.podbuilders;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (email.equals(authmail) && pass.equals(authpass)){
 
+            Intent intent = new Intent(this,Navigation.class);
+            startActivity(intent);
 
         }
         else {
--- app/src/main/java/com/example/adambenyahia/podbuilders/Navigation.java
+++ app/src/main/java/com/example/adambenyahia/podbuilders/Navigation.java
@@ -80,13 +80,13 @@ public class Navigation extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.view_gallery) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.our_locations) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.testimonials) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.contact) {
 
         } else if (id == R.id.nav_share) {
 
